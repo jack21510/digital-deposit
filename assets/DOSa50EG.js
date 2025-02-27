2eab64d553d8c46a80d2a9d9f665f79ca49d8718
jackchen <jackchen@easontech.com.tw>
2025-02-27 15:10:52 +0800

    test:首頁無限load問題

---
+++ assets/DOSa50EG.js
@@ -0,0 +1,1 @@
+import"./DWY0TjIW.js";const t=""+new URL("bird-3.CmknHYzL.svg",import.meta.url).href;export{t as _};
---
+++ assets/DTAMRV-5.js
@@ -0,0 +1,1 @@
+import"./DWY0TjIW.js";const e=""+new URL("invest-helper-land.D-hrdpKi.svg",import.meta.url).href;export{e as _};
